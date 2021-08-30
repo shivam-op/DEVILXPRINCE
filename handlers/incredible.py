7dc57557918c87ec41af8d67cd7d0c8e6da2fc5a
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-08-30 20:14:45 +0530

    Update incredible.py

--- handlers/incredible.py
+++ handlers/incredible.py
@@ -14,16 +14,13 @@ async def help_(client: Client, message: Message):
 **For all in group**
 
 - `/play <song name> - play song you requested 
-
 - `/song <song name> - download songs you want quickly
 
  
 ** Admin only**🔥😏
 
-
 - `/pause - pause song play
-
 - `/resume - resume song play
-
 - `/skip - play the next song
+- `/end - end song
 
