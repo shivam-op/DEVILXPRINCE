dd4f287a878787b32e04a58ab404278879df6237
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-08-30 22:26:04 +0530

    Update gcast.py

--- handlers/gcast.py
+++ handlers/gcast.py
@@ -1,5 +1,3 @@
-
-
 import asyncio
 import regex
 from pyrogram import Client
@@ -11,8 +9,8 @@ from aiohttp import ClientSession
 from config import SUDO_USERS, BOT_TOKEN
 from pyrogram.errors import UserAlreadyParticipant
 
-from .services.callsmusic.callsmusic import client as USER
-from camillavcbot2.0.config import SUDO_USERS
+from callsmusic.callsmusic import client as USER
+from config import SUDO_USERS
 
 @Client.on_message(filters.command(["broadcast"]))
 async def broadcast(_, message: Message):
@@ -21,7 +19,7 @@ async def broadcast(_, message: Message):
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`Starting a broadcast...`")
+        wtf = await message.reply("Starting a broadcast...")
         if not message.reply_to_message:
             await wtf.edit("Please Reply to a Message to broadcast!")
             return
@@ -30,13 +28,11 @@ async def broadcast(_, message: Message):
             try:
                 await USER.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
+                await wtf.edit(f"broadcasting... \n\n**Sent to:** {sent} Chats \n**Failed in:** {failed} Chats")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-                #await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
+                #await wtf.edit(f"broadcasting... \n\n**Sent to:** {sent} Chats \n**Failed in:** {failed} Chats")
                 
             
-        await message.reply_text(f"`Broadcast Finished ` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
-
-
+        await message.reply_text(f"Broadcast Finished  \n\n**Sent to:** {sent} Chats \n**Failed in:** {failed} Chats")
