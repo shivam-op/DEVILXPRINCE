4a179560af61528204be6393c840c6418631dc8b
darkophacker0987 <82193878+darkophacker0987@users.noreply.github.com>
2021-08-31 21:31:28 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -118,13 +118,13 @@ async def play(_, message: Message):
                 try:
                     await USER.join_chat(invitelink)
                     await USER.send_message(
-                        message.chat.id, "**camila Music assistant joined this group for play music 🎵**")
+                        message.chat.id, "**DEVILXPRINCE Music assistant joined this group for play music 🎵**")
 
                 except UserAlreadyParticipant:
                     pass
                 except Exception:
                     await lel.edit(
-                        f"<b>🛑 Flood Wait Error 🛑</b> \n\Hey {user.first_name},@camilaowner assistant userbot couldn't join your group due to heavy join requests @camila_support. Make sure userbot is not banned in group and try again later!")
+                        f"<b>🛑 Flood Wait Error 🛑</b> \n\Hey {user.first_name},@DEViL_X_PRiNCEx assistant userbot couldn't join your group due to heavy join requests @camila_support. Make sure userbot is not banned in group and try again later!")
     try:
         await USER.get_chat(chid)
     except:
@@ -153,7 +153,7 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton(
                         text="Channel 🔊",
-                        url="https://t.me/BONDOFBESTIZZ")
+                        url="https://t.me/cyber_hunters_status")
                    
                 ]
             ]
@@ -191,7 +191,7 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton(
                         text="Channel 🔊",
-                        url="https://t.me/BONDOFBESTIZZ")
+                        url="https://t.me/cyber_hunters_status")
                    
                 ]
             ]
@@ -206,7 +206,7 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton(
                         text="Channel 🔊",
-                        url="https://t.me/BONDOFBESTIZZ")
+                        url="https://t.me/cyber_hunters_status")
                    
                 ]
             ]
@@ -256,7 +256,7 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton(
                         text="Channel 🔊",
-                        url="https://t.me/BONDOFBESTIZZ")
+                        url="https://t.me/cyber_hunters_status")
                    
                 ]
             ]
